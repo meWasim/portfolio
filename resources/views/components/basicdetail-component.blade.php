34a8b50a2a8c735121487a9d394507bf29290c75
Wasim Ansari <alfwasim@gmail.com>
2023-10-14 16:34:40 +0530

    #

---
+++ resources/views/components/basicdetail-component.blade.php
@@ -0,0 +1,56 @@
+<div class="container-fluid">
+    <div id="about" class="row about-section">
+        <div class="col-lg-4 about-card">
+            <h3 class="font-weight-light">Who am I ?</h3>
+            <span class="line mb-5"></span>
+            <h5 class="mb-3">A Backend Developer</h5>
+            <ul class="mt-20">
+                <li>Currently working in Matainja Technology pvt ltd</li>
+                <li>Kolkata Based Startup Service Based Company</li>
+            </ul>
+            <button class="btn btn-outline-danger"><i class="icon-down-circled2 "></i>Download My CV</button>
+        </div>
+        <div class="col-lg-4 about-card">
+            <h3 class="font-weight-light">Personal Info</h3>
+            <span class="line mb-5"></span>
+            <ul class="mt40 info list-unstyled">
+                <li><span>Birthdate</span> : 27/12/2000</li>
+                <li><span>Email</span> : alfwasim@gmail.com</li>
+                <li><span>Phone</span> : +919661146360</li>
+                <li><span>Address</span> : kolkata(West Bengal)</li>
+            </ul>
+            <ul class="social-icons pt-3">
+                <li class="social-item"><a class="social-link" href="#"><i class="bi bi-facebook" aria-hidden="true"></i></a></li>
+                <li class="social-item"><a class="social-link" href="#"><i class="bi bi-twitter" aria-hidden="true"></i></a></li>
+                <li class="social-item"><a class="social-link" href="#"><i class="bi bi-google" aria-hidden="true"></i></a></li>
+                <li class="social-item"><a class="social-link" href="#"><i class="bi bi-instagram" aria-hidden="true"></i></a></li>
+                <li class="social-item"><a class="social-link" href="#"><i class="bi bi-github" aria-hidden="true"></i></a></li>
+            </ul>
+        </div>
+        <div class="col-lg-4 about-card">
+            <h3 class="font-weight-light">My Expertise</h3>
+            <span class="line mb-5"></span>
+            <div class="row">
+                <div class="col-1 text-danger pt-1"><i class="ti-widget icon-lg"></i></div>
+                <div class="col-10 ml-auto mr-3">
+                    <h6>UX Design</h6>
+                    <hr>
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-1 text-danger pt-1"><i class="ti-paint-bucket icon-lg"></i></div>
+                <div class="col-10 ml-auto mr-3">
+                    <h6>Web Development</h6>
+                    <hr>
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-1 text-danger pt-1"><i class="ti-stats-up icon-lg"></i></div>
+                <div class="col-10 ml-auto mr-3">
+                    <h6>DSoftware Engineering</h6>
+                    <hr>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
---
+++ resources/views/components/header-component.blade.php
@@ -0,0 +1,17 @@
+<header class="header">
+        <div class="container">
+            <ul class="social-icons pt-3">
+                <li class="social-item"><a class="social-link text-light" href="#"><i class="bi bi-linkedin" aria-hidden="true"></i></a></li>
+                <li class="social-item"><a class="social-link text-light" href="#"><i class="bi bi-twitter" aria-hidden="true"></i></a></li>
+                <li class="social-item"><a class="social-link text-light" href="#"><i class="bi bi-google" aria-hidden="true"></i></a></li>
+                <li class="social-item"><a class="social-link text-light" href="#"><i class="bi bi-instagram" aria-hidden="true"></i></a></li>
+                <li class="social-item"><a class="social-link text-light" href="https://github.com/meWasim" target="_blank"><i class="bi bi-github" aria-hidden="true"></i></a></li>
+            </ul>
+            <div class="header-content">
+                <h4 class="header-subtitle">Hello, I am</h4>
+                <h1 class="header-title">WASIM ANSARI</h1>
+                <h6 class="header-mono">Backend Developer</h6>
+                <button class="btn btn-primary btn-rounded"><i class="bi bi-printer pr-2"></i><a href="https://flowcv.com/resume/lphp12e4sr" target="_blank"  style="text-decoration: none; color:white">view Resume</a></button>
+            </div>
+        </div>
+    </header>
